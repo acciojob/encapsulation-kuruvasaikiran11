2f8915ce2cb370ef8cc9b1a3bee9abad605ebda5
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2022-12-14 22:37:17 +0530

    Encapsulation Final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,9 @@ package com.driver;
 public class Main {
   public static void main(String args[]){
       RWOnly rw = new RWOnly();
-      rw.name = "Sai";
+      //name has private access in com.driver.RWOnly
+      //rw.name = "Sai";
+      rw.setName("Sai");
+      System.out.println(rw.getName());
     }
 }
